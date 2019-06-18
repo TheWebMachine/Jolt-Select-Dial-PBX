7668df5be1ae5f17dada7b4964b08cdbc2acfa66
TheWebMachine <1045052+TheWebMachine@users.noreply.github.com>
2019-06-18 02:46:27 -0500

    Update freepbx_call.php
    Rewrote bootstrap and function to retrieve all users in newer FreePBX versions (13+). This helps ensure proper security by preventing the Origination extension from being anything other than a valid extension on the PBX (prevents "middleman/proxy" style attacks, especially on internet exposed systems).

--- freepbx_call.php
+++ freepbx_call.php
@@ -7,9 +7,9 @@
  *  https://github.com/Jolt1/Jolt-Select-Dial-PBX
  *
  * Instructions:
- *      Place a copy of this file in the same place as the call.php file
- *      from the Jolt repo. Modify the Jolt extension URL to call this file
- *      i.e.  http://pbx_address/freepbx_call.php
+ *      Place a copy of this file in /var/www/html. Modify the Jolt
+ *		extension URL to call this file.
+ *		i.e.  https://pbx_address/freepbx_call.php
  *
  * License:
  *    The original MIT license terms apply
@@ -20,10 +20,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// load FreeBPX bootstrap environment, requires FreePBX 2.9+
-if (!@include_once(getenv('FREEPBX_CONF') ? getenv('FREEPBX_CONF') : '/etc/freepbx.conf')) {
-	include_once('/etc/asterisk/freepbx.conf');
-}
+// FreeBPX bootstrap, works on FreePBX 13+ 
+$bootstrap_settings = array();
+$bootstrap_settings['freepbx_auth'] = false;
+include '/etc/freepbx.conf';
+
+
 
 global $db;  		// FreePBX asterisk database connector
 global $amp_conf;	// don't think we need this
@@ -56,9 +58,10 @@ elseif (isset($_GET['phone'])) {
 $number = filter_var($number, FILTER_SANITIZE_NUMBER_INT);
 $number = preg_replace("/[^0-9,.]/", "", $number);
 
-/**** THIS IS BROKEN IN FreePBX 14 ****
+
 // Get a 2D array of all FreePBX extension details
-$extension_array = core_users_list();         
+$get_all = FALSE;
+$extension_array = FreePBX::Core()->listUsers($get_all);
 
 // Convert the 2d array to a 1d array of just extension numbers
 foreach ($extension_array as $bar) {
@@ -67,15 +70,14 @@ foreach ($extension_array as $bar) {
 
 // Check if the extension number passed to the script is a valid system extension
 if (in_array($ext,$ext_list)) {
-****/
+
 	$strChannel = "local/".$ext."@$strContext";;    // Still need to carry the ext forward, tho
-/**** THIS IS BROKEN IN FreePBX 14 ****
 }
 else {
-	echo "Error. $ext is not a valid extension";
+	echo "Error: '$ext' is not a valid extension!";
 	exit;
 }
-****/
+
 
 // Construct an array for dialling
 $dial = array();
@@ -87,7 +89,7 @@ $dial['Async'] = $strAsync;
 $dial['Timeout'] = $strWaitTime;
 $dial['CallerID'] = '"Click to dial" <'.$number.'>';
 
-echo "Bridging extension $ext to number $number." ;
+echo "Bridging extension $ext to number $number..." ;
 
 // using $astman class from bootstrap, originate the call
 $astman->Originate($dial);
